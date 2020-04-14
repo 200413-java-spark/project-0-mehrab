39d8d603f8b7ae2e562041d728c703ba0f8d5674
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-14 10:38:58 -0500

    updated calc to use Java Scanner obj to fix char issue

--- Calc.java
+++ Calc.java
@@ -1,11 +1,24 @@
+import java.util.Scanner; 
 class Calc {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]);
-        String operator = args[1];
-        int b = Integer.parseInt(args[2]);
 
-        if (operator.equals("+")) {
-            System.out.println(a + b);
+        Scanner calc_input = new Scanner(System.in);
+
+        System.out.println("Enter operand 1");
+
+        int operand_a=calc_input.nextInt();        
+
+        System.out.println("Enter operand 2");
+        int operand_b = calc_input.nextInt();
+
+        System.out.println("Enter operator ex: +, -, *, /");
+        Character operator = calc_input.next().charAt(0);
+
+        if (operator.equals('*')) {
+            System.out.println(operand_a * operand_b);
         }
+        
+        
+
     }
 }
